7792a860381b035551c99a8189177d6bf6e4d1ab
towander <137wander@gmail.com>
2018-04-28 10:56:22 +0800

    test

--- xip.c
+++ xip.c
@@ -171,7 +171,9 @@ static inline size_t memcpy_to_nvmm(char *kmem, loff_t offset,
 						offset, buf, bytes);
 	}
 	/* dedup start */
+	printk("kmem+offset length:%d",(int)sizeof(*(kmem+offset)));
 	printk("kmem+offset:%s\n",kmem+offset);
+	printk("kmem length:%d",(int)sizeof(*kmem));
 	printk("kmem:%s\n",kmem);
 	/* dedup end */
 	return copied;
@@ -215,7 +217,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		PMFS_END_TIMING(memcpy_w_t, memcpy_time);
 
 		/* dedup start */
-		printk("xmem:%s\n",(char *)xmem);
+		// printk("xmem:%s\n",(char *)xmem);
 		/* dedup end */
 
 		/* if start or end dest address is not 8 byte aligned, 
@@ -432,7 +434,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	
 	/* dedup start */
 	// printk("buf:%s\n",buf);
-	printk("buf length:%d\n",(int)sizeof(buf));
+	printk("buf length:%d\n",(int)sizeof(*buf));
 	/* use strncpy create fingerprint */
 	// char fingerprint[128];
 	// memcpy(fingerprint,buf+3968,128);
