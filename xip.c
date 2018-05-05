f3f849e021ff0ca7c7bc04376e46487680b439a5
towander <137wander@gmail.com>
2018-05-05 17:50:57 +0800

    test

--- xip.c
+++ xip.c
@@ -257,14 +257,17 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 	}
 
 	//dedup regin start
-	for(i=0;i<128;i++)
-	{
-		strncpy(temp,buf+i*16,16);
-		hashing += (unsigned long long)temp;
-		hashing += (hashing << 8);
-		hashing ^= (hashing >> 2);
-	}
-	printk("hashing:%s\n",hashing);
+	// for(i=0;i<128;i++)
+	// {
+	// 	strncpy(temp,buf+i*16,16);
+	// 	hashing += (unsigned long long)temp;
+	// 	hashing += (hashing << 8);
+	// 	hashing ^= (hashing >> 2);
+	// }
+	strncpy(temp,buf,16);
+	printk("buf:%s\n",buf);
+	printk("hashing:%s\n",temp);
+	printk("hashing:%llu\n",temp);
 	//regin end
 
 	PMFS_END_TIMING(internal_write_t, write_time);
