52debdc7d008e6edb11512110ef83d2f16b6f308
towander <137wander@gmail.com>
2018-05-05 21:46:49 +0800

    test

--- xip.c
+++ xip.c
@@ -271,6 +271,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 	// 	hashing += (hashing << 8);
 	// 	hashing ^= (hashing >> 2);
 	// }
+	
 	strncpy(temp,buf+16,16);
 	printk("buf:%s\n",buf);
 	printk("hashing:%s\n",temp);
