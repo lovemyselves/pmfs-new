a428b7705cff641c73a085dadd9276a64265dd9f
towander <137wander@gmail.com>
2018-05-07 10:34:51 +0800

    test

--- xip.c
+++ xip.c
@@ -225,11 +225,11 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 	INIT_LIST_HEAD(&hash_map_ppn_entry->list);
 	list_add_tail(&hash_map_ppn_entry->list, &hash_map_ppn_list);
 	/* hash_map_ppn_entry ponit reuse for traverse */
-	list_for_each_entry(hash_map_ppn_entry,&hash_map_ppn_list,list)
-	{
-		printk("hashing in this map:%lu\n",hash_map_ppn_entry->hashing);
-		printk("count in this map:%lu\n",hash_map_ppn_entry->count);
-	}
+	// list_for_each_entry(hash_map_ppn_entry,&hash_map_ppn_list,list)
+	// {
+	// 	printk("hashing in this map:%lu\n",hash_map_ppn_entry->hashing);
+	// 	printk("count in this map:%u\n",hash_map_ppn_entry->count);
+	// }
 	
 	//end
 
