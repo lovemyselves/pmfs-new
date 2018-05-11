1780d8c538be1f6a9b42dc824e05ee1dadf622ca
towander <137wander@gmail.com>
2018-05-11 19:11:56 +0800

    test

--- xip.c
+++ xip.c
@@ -443,7 +443,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	block = pmfs_find_data_block(inode, start_blk);
 
 	//dedup insert start
-	printk("ino:%lu\n",inode->i_ino);
+	// printk("ino:%lu\n",inode->i_ino);
 	// printk("offset:%lu\n",offset);
 	// printk("pos:%llu\n",pos);
 	// printk("blocksize:%u",sb->s_blocksize_bits);
