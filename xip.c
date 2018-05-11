ebcd432e540ccb384171a0470a4dfd651ee3dd82
towander <137wander@gmail.com>
2018-05-11 10:21:29 +0800

    test

--- xip.c
+++ xip.c
@@ -447,6 +447,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	// printk("end_blk:%lu\n",end_blk);
 	// printk("block:%llu\n",block);
 	printk("start_blk>>5:%lu\n",start_blk>>5);
+	printk("buf:%s\n",buf);
 	//end
 
 	/* Referring to the inode's block size, not 4K */
@@ -665,7 +666,6 @@ int pmfs_get_xip_mem(struct address_space *mapping, pgoff_t pgoff, int create,
 	int rc;
 	sector_t block = 0;
 	struct inode *inode = mapping->host;
-	int i;
 
 	rc = __pmfs_get_block(inode, pgoff, create, &block);
 	if (rc) {
@@ -689,7 +689,7 @@ int pmfs_get_xip_mem(struct address_space *mapping, pgoff_t pgoff, int create,
 	printk("block value:%lu\n",block>>12);
 	printk("pfn:%lu\n",*pfn);
 	printk("kmem:%lu\n",(unsigned long)*kmem);
-	for(i=0;i<64;i++) printk("%s",(char*)(*kmem + i));
+	printk("%s",(char*)*kmem);
 	/* end */
 
 	return 0;
