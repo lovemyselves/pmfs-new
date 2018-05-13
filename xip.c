fb4b889b1437d8a445c143788239584e6694b50c
towander <137wander@gmail.com>
2018-05-13 17:04:41 +0800

    test

--- xip.c
+++ xip.c
@@ -30,7 +30,7 @@
 // l_map_p = kmalloc(sizeof(struct lpn_map_ppn), GFP_KERNEL);
 static LIST_HEAD(hash_map_addr_list);
 struct list_head *last_hit;
-bool find_flag = false;
+
 /* claim end */
 
 static ssize_t
@@ -206,6 +206,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 	ssize_t     written = 0;
 	struct pmfs_inode *pi;
 	timing_t memcpy_time, write_time;
+	bool find_flag = false;
 
 	PMFS_START_TIMING(internal_write_t, write_time);
 	pi = pmfs_get_inode(sb, inode->i_ino);
