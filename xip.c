a6927b7f95776a55de72c2e7252eb37fe06999d2
towander <137wander@gmail.com>
2018-05-13 22:22:25 +0800

    test

--- xip.c
+++ xip.c
@@ -261,7 +261,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		{
 			unsigned count = ++ list_entry(last_hit.next,struct hash_map_addr,list)->count;
 			last_hit.next = last_hit.next->next;
-			printk("fast hit, reference count:%u\n", count);
+			// printk("fast hit, reference count:%u\n", count);
 			goto find;
 		}
 		
@@ -277,14 +277,14 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 				// find_flag = 1;
 				last_hit.next = hash_map_addr_entry->list.next;
 				find_flag = true;
-				printk("general hit, reference count:%u\n", hash_map_addr_entry->count);
+				// printk("general hit, reference count:%u\n", hash_map_addr_entry->count);
 				goto find;
 			}	
 		}
 		
 		// not dup, insert new index
 		find_flag = false;
-		printk("not hash hit\n");
+		// printk("not hash hit\n");
 		hash_map_addr_temp->hashing = hashing;
 		hash_map_addr_temp->count = 1;
 		// hash_map_addr_temp->addr = kmalloc(6*sizeof(char), GFP_KERNEL);
