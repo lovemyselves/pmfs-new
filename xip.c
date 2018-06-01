c9892076acc7ee9c5726016ab199765766f993b5
towander <137wander@gmail.com>
2018-06-01 10:22:43 +0800

    test

--- xip.c
+++ xip.c
@@ -31,6 +31,48 @@ static LIST_HEAD(hash_map_addr_list);
 static LIST_HEAD(last_hit);
 bool find_flag = 0;
 struct rb_root root = RB_ROOT;
+
+/*
+	dedup rbtree function
+*/
+struct hash_map_addr *rb_search_node(struct rb_root *root, unsigned hashing)
+{
+	struct rb_node *entry_node = root->rb_node;
+	int result;
+	struct hash_map_addr *hash_map_addr_entry;
+	
+	while(entry_node){
+		hash_map_addr_entry = rb_entry(entry_node, struct hash_map_addr, node);
+		result = hashing - hash_map_addr_entry->hashing; 
+		if(result < 0)
+			entry_node = entry_node->rb_left;
+		else if(result > 0)
+			entry_node = entry_node->rb_right;
+		else
+			return hash_map_addr_entry;
+	}
+	return NULL;
+}
+
+void rb_insert_node(struct rb_root *root, struct hash_map_addr *hash_map_addr_entry)
+{
+	struct rb_node **entry_node = &(root->rb_node);
+	struct rb_node *parent = NULL;
+	struct hash_map_addr *hash_map_addr_temp;
+	int result;
+
+	while(*entry_node){
+		parent = *entry_node;
+		hash_map_addr_temp = rb_entry(*entry_node, struct hash_map_addr, node);
+		result = hash_map_addr_entry->hashing - hash_map_addr_temp->hashing;
+		if(result < 0)
+			entry_node = &(*entry_node)->rb_left;
+		else
+			entry_node = &(*entry_node)->rb_right;
+	}
+	rb_link_node(&(hash_map_addr_entry->node), parent, entry_node);
+	rb_insert_color(&(hash_map_addr_entry->node), root);
+}
 /* claim end */
 
 static ssize_t
@@ -738,46 +780,4 @@ int pmfs_xip_file_mmap(struct file *file, struct vm_area_struct *vma)
 			vma->vm_flags, pgprot_val(vma->vm_page_prot));
 
 	return 0;
-}
-
-/*
-	dedup rbtree function
-*/
-struct hash_map_addr *rb_search_node(struct rb_root *root, unsigned hashing)
-{
-	struct rb_node *entry_node = root->rb_node;
-	int result;
-	struct hash_map_addr *hash_map_addr_entry;
-	
-	while(entry_node){
-		hash_map_addr_entry = rb_entry(entry_node, struct hash_map_addr, node);
-		result = hashing - hash_map_addr_entry->hashing; 
-		if(result < 0)
-			entry_node = entry_node->rb_left;
-		else if(result > 0)
-			entry_node = entry_node->rb_right;
-		else
-			return hash_map_addr_entry;
-	}
-	return NULL;
-}
-
-void rb_insert_node(struct rb_root *root, struct hash_map_addr *hash_map_addr_entry)
-{
-	struct rb_node **entry_node = &(root->rb_node);
-	struct rb_node *parent = NULL;
-	struct hash_map_addr *hash_map_addr_temp;
-	int result;
-
-	while(*entry_node){
-		parent = *entry_node;
-		hash_map_addr_temp = rb_entry(*entry_node, struct hash_map_addr, node);
-		result = hash_map_addr_entry->hashing - hash_map_addr_temp->hashing;
-		if(result < 0)
-			entry_node = &(*entry_node)->rb_left;
-		else
-			entry_node = &(*entry_node)->rb_right;
-	}
-	rb_link_node(&(hash_map_addr_entry->node), parent, entry_node);
-	rb_insert_color(&(hash_map_addr_entry->node), root);
 }
