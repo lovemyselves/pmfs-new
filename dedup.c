c8a7c9246d77d70684d189dc52b00f1a6180452c
towander <137wander@gmail.com>
2018-06-01 09:31:14 +0800

    test

--- dedup.c
+++ dedup.c
@@ -1,15 +1,15 @@
 #include <linux/list.h>
 #include <linux/rbtree.h>
 
-struct hash_map_addr{
-    unsigned hashing;
-    unsigned long hashing_md5;
-    void *addr;
-    unsigned int count;
-    struct list_head list; 
-};
+// struct hash_map_addr{
+//     unsigned hashing;
+//     unsigned long hashing_md5;
+//     void *addr;
+//     unsigned int count;
+//     struct list_head list; 
+// };
 
-struct __hash_map_addr{
+struct hash_map_addr{
     unsigned hashing;
     void *hashing_md5;
     void *addr;
