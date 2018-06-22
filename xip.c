d6c0da14a82e4d0e54d3053bfd1be2912c656d14
towander <137wander@gmail.com>
2018-06-22 15:14:15 +0800

    analysis

--- xip.c
+++ xip.c
@@ -565,6 +565,10 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	i = count;
 	while(i>0){
 		// __copy_from_user(xmem, buf, pmfs_inode_blk_size(pi));
+		if (access_ok(VERIFY_READ, buf+count-i, 4096)){
+			__copy_from_user(xmem, buf+count-i, 4096);
+			printk("xmem:%s",xmem);
+		}	
 		for(j=0;i<128;j++)
 		{
 			// memcpy(temp,(char*)buf+j*sizeof(unsigned),sizeof(unsigned));
