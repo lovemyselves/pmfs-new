eed3bf84b8a183498f236c56d53d13b63388423a
towander <137wander@gmail.com>
2018-06-22 16:38:05 +0800

    analysis

--- xip.c
+++ xip.c
@@ -486,7 +486,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	unsigned hashing = 0;
 	unsigned long *temp = kmalloc(sizeof(unsigned long), GFP_KERNEL);
 	int i=0,j;
-	void *xmem;
+	char *xmem;
 
 	struct hash_map_addr *hash_map_addr_entry, *hash_map_addr_temp;
 	hash_map_addr_temp = kmalloc(sizeof(*hash_map_addr_temp), GFP_KERNEL);
@@ -566,10 +566,10 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	i = count;
 	
 	while(i>0){
-		// __copy_from_user(xmem, buf, pmfs_inode_blk_size(pi));
-		// copy_from_user(xmem, buf, 4096);	
+		// __copy_from_user(xmem, buf, pmfs_inode_blk_size(pi));	
 		if (access_ok(VERIFY_READ, buf, pmfs_inode_blk_size(pi))){
 			printk("test read");
+			copy_from_user(xmem, buf, pmfs_inode_blk_size(pi));
 		}
 		for(j=0;i<128;j++)
 		{
