06fdf6086049a5b1beb74c0b4af3376c22c8ae8b
towander <137wander@gmail.com>
2018-06-23 19:27:35 +0800

    analysis

--- xip.c
+++ xip.c
@@ -72,10 +72,12 @@ void rb_insert_node(struct rb_root *root, struct hash_map_addr *hash_map_addr_ne
 		else{
 			printk("hashing1:%lu",hash_map_addr_new->hashing);
 			printk("hashing2:%lu",hash_map_addr_entry->hashing);
-			printk("hashing accident!");
+			hash_map_addr_entry = rb_search_node(root, hashing);
+			if(hash_map_addr_entry){printk("hashing accident!");}
 			return;
 		}
 	}
+	printk("success come here!");
 	rb_link_node(&hash_map_addr_new->node, parent, entry_node);
 	rb_insert_color(&hash_map_addr_new->node, root);
 }
@@ -624,7 +626,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 		hash_map_addr_temp->addr = (void*)xmem;
 
 		INIT_LIST_HEAD(&hash_map_addr_temp->list);
-		// list_add_tail(&hash_map_addr_temp->list, &hash_map_addr_list);
+		list_add_tail(&hash_map_addr_temp->list, &hash_map_addr_list);
 		rb_insert_node(&root, hash_map_addr_temp);
 		
 		find:
