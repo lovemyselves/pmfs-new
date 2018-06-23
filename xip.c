606710c5554f2641ef743b4af0cf011a1a020d1c
towander <137wander@gmail.com>
2018-06-23 10:45:11 +0800

    analysis

--- xip.c
+++ xip.c
@@ -567,26 +567,38 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	do{	
 		if (i>pmfs_inode_blk_size(pi)){
 			copy_from_user(xmem, buf, pmfs_inode_blk_size(pi));
-			printk("i:%d",i);
-		}
-		else{
-			copy_from_user(xmem, buf, i);
-			printk("small i:%d",i);
-		}
-		for(j=0;i<128;j++)
-		{
+			printk("i:%u",i);
+			for(j=0;j<128;j++){
 			memcpy(temp,xmem+j*sizeof(unsigned),sizeof(unsigned));
 			hashing += *temp;
 			hashing += (hashing << 3);
 			hashing ^= (hashing >> 2);
+			}
 		}
-		printk("pmfs_inode_blk_size(pi):%u",pmfs_inode_blk_size(pi));
-		printk("count:%u",count);
+		else{
+			copy_from_user(xmem, buf, i);
+			printk("last i:%u",i);
+			for(j=0;j<128&&(j<i/sizeof(unsigned));j++){
+			memcpy(temp,xmem+j*sizeof(unsigned),sizeof(unsigned));
+			hashing += *temp;
+			hashing += (hashing << 3);
+			hashing ^= (hashing >> 2);
+			}
+		}
+		// for(j=0;i<128;j++)
+		// {
+		// 	memcpy(temp,xmem+j*sizeof(unsigned),sizeof(unsigned));
+		// 	hashing += *temp;
+		// 	hashing += (hashing << 3);
+		// 	hashing ^= (hashing >> 2);
+		// }
+		// printk("pmfs_inode_blk_size(pi):%u",pmfs_inode_blk_size(pi));
+		// printk("count:%u",count);
 		printk("hashing:%u",hashing);
 		printk("\n");
 		
 		i -= pmfs_inode_blk_size(pi);
-	}while(i<count&&i);
+	}while(i-1<count);
 
 	/* We avoid zeroing the alloc'd range, which is going to be overwritten
 	 * by this system call anyway */
