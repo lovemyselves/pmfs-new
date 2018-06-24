b1159c0ecf3acd92e4f863ad269431366799076d
towander <137wander@gmail.com>
2018-06-24 11:29:58 +0800

    analysis

--- xip.c
+++ xip.c
@@ -384,7 +384,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 	new_list = hash_map_addr_list.next;
 	for(i=0;i<8;i++){
 		rb_insert_node(&root, list_entry(new_list->next, struct hash_map_addr, list));
-		new_list = new_list->next;
+		new_list = new_list->prev;
 		printk("new rbtree node hashing:%lu",list_entry(new_list->next, struct hash_map_addr, list)->hashing);
 		printk("======================================");
 	}
