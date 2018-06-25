d09b5ea5e168098fae2c6df11828672c7287b70f
towander <137wander@gmail.com>
2018-06-25 21:28:50 +0800

    analysis

--- xip.c
+++ xip.c
@@ -516,10 +516,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	timing_t xip_write_time, xip_write_fast_time;
 
 	//dedup claiming start
-	size_t i,j;
-	size_t hashing;
-	
-	
+	size_t i,j;	
 	//end
 
 	PMFS_START_TIMING(xip_write_t, xip_write_time);
@@ -597,11 +594,12 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	do{	
 		
 		char const *data_block = buf + count - i;
+		size_t hashing = 0;
 		struct hash_map_addr *hash_map_addr_temp;
 		struct hash_map_addr *hash_map_addr_entry;
 		char *xmem = kmalloc(pmfs_inode_blk_size(pi),GFP_KERNEL);
 		hash_map_addr_temp = kmalloc(sizeof(*hash_map_addr_temp), GFP_KERNEL);
-		hashing = 0;
+		
 
 		if (i>pmfs_inode_blk_size(pi)){
 			copy_from_user(xmem, data_block, pmfs_inode_blk_size(pi));
