b67e455302ebcea7c1a5d3d87c2d8bfbc829cb44
towander <137wander@gmail.com>
2018-06-25 11:02:11 +0800

    analysis

--- xip.c
+++ xip.c
@@ -583,7 +583,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	i = count;
 	do{	
 		size_t hashing = 0;
-		void *data_block = buf+count-i;
+		void const *data_block = buf+count-i;
 		struct hash_map_addr *hash_map_addr_temp;
 		unsigned long *temp = kmalloc(sizeof(unsigned long), GFP_KERNEL);
 		// char *xmem = kmalloc(pmfs_inode_blk_size(pi),GFP_KERNEL);
