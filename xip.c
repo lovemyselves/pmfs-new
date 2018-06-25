e230ba18e1e48e9b90662b33649d56f6633c86e7
towander <137wander@gmail.com>
2018-06-25 22:25:26 +0800

    analysis

--- xip.c
+++ xip.c
@@ -597,28 +597,28 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 		struct hash_map_addr *hash_map_addr_temp;
 		struct hash_map_addr *hash_map_addr_entry;
 		size_t *temp = kmalloc(sizeof(size_t),GFP_KERNEL);
-		char const *xmem = kmalloc(pmfs_inode_blk_size(pi),GFP_KERNEL);
+		void *kmem = kmalloc(pmfs_inode_blk_size(pi),GFP_KERNEL);
 		hash_map_addr_temp = kmalloc(sizeof(*hash_map_addr_temp), GFP_KERNEL);
 		
 
 		if (i>pmfs_inode_blk_size(pi)){
-			copy_from_user(xmem, data_block, pmfs_inode_blk_size(pi));
+			copy_from_user(kmem, data_block, pmfs_inode_blk_size(pi));
 			printk("i:%lu",i);
 			for(j=0;j<128;j++){
-				memcpy(temp,xmem+j*sizeof(size_t),sizeof(size_t));	
-				hashing += temp;
+				memcpy(temp,kmem+j*sizeof(size_t),sizeof(size_t));	
+				hashing += *temp;
 				hashing += (hashing << 3);
 				hashing ^= (hashing >> 2);
 			}
 			// printk("compute result of hashing:%lu",hashing);
 		}
 		else{
-			copy_from_user(xmem, data_block, i);
+			copy_from_user(kmem, data_block, i);
 			printk("last i:%lu",i);
 			// printk("data_block:%lu",(size_t)data_block);
 			for(j=0;j<128&&(j<i/sizeof(size_t));j++){
-				memcpy(temp,xmem+j*sizeof(size_t),sizeof(size_t));	
-				xmem += temp;
+				memcpy(temp,kmem+j*sizeof(size_t),sizeof(size_t));	
+				hashing += *temp;
 				hashing += (hashing << 3);
 				hashing ^= (hashing >> 2);
 			}
