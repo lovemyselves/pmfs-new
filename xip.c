fe3d08b4e3a286524efcb31e00f7a1d9f674f11a
towander <137wander@gmail.com>
2018-07-09 22:12:05 +0800

    fix

--- xip.c
+++ xip.c
@@ -36,9 +36,9 @@ struct rb_root root = RB_ROOT;
 struct list_head *last_ref;
 bool ref_find_flag = false;
 struct rb_root ref_root = RB_ROOT;
-struct list_head *ref_map_list;
+struct list_head ref_map_list;
 
-INIT_LIST_HEAD(ref_map_list);
+INIT_LIST_HEAD(&ref_map_list);
 
 /*
 	dedup rbtree function
