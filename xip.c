697142d2b9f49b16d697722767a8cce30d1b441b
towander <137wander@gmail.com>
2018-07-12 21:25:41 +0800

    fix

--- xip.c
+++ xip.c
@@ -758,6 +758,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 			hashing ^= (hashing >> 2);
 		}
 
+		printk("hashing:%lu",hashing);
 		hash_map_addr_temp->hashing = hashing;
 		hash_map_addr_temp->count = 1;
 		hash_map_addr_temp->addr = xmem;
