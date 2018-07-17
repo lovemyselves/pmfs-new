1cab6efa0d737a678f26df31d9640d5e6275be14
towander <137wander@gmail.com>
2018-07-17 16:40:14 +0800

    test

--- xip.c
+++ xip.c
@@ -803,10 +803,10 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 			/*add reference content */
 		}
 		else{
-			dedup_interval <<= 1;
-			if(dedup_interval==32)
-				dedup_interval=31;
-			// dedup_interval = (31 & ((dedup_interval<<1) - 1)) + 1;
+			// dedup_interval <<= 1;
+			// if(dedup_interval==32)
+			// 	dedup_interval=31;
+			dedup_interval = (31 & ((dedup_interval<<1) - 1)) + 1;
 			printk("dedup_interval:%lu",dedup_interval);
 			// dedup_interval = 31;
 		}
