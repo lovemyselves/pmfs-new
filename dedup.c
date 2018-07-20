fff8630815a70684bb56396bac90a15aef1eac4c
towander <137wander@gmail.com>
2018-07-20 21:37:32 +0800

    test

--- dedup.c
+++ dedup.c
@@ -1,16 +1,5 @@
 #include <linux/list.h>
 #include <linux/rbtree.h>
-#include <crypto/hash.h>
-#include <linux/err.h>
-#include <linux/scatterlist.h>
-
-// struct hash_map_addr{
-//     unsigned hashing;
-//     unsigned long hashing_md5;
-//     void *addr;
-//     unsigned int count;
-//     struct list_head list; 
-// };
 
 struct hash_map_addr{
     size_t hashing;
@@ -34,28 +23,3 @@ struct ref_map{
     struct list_head list;
     struct rb_node node;
 };
-
-
-struct scatterlist sg[2];
-char result[128];
-struct crypto_ahash *tfm;
-struct ahash_request *req;
-	
-tfm = crypto_alloc_ahash("md5", 0, CRYPTO_ALG_ASYNC);
-if (IS_ERR(tfm))
-	fail();
-	
-/* ... set up the scatterlists ... */
-
-req = ahash_request_alloc(tfm, GFP_ATOMIC);
-if (!req)
-	fail();
-
-ahash_request_set_callback(req, 0, NULL, NULL);
-ahash_request_set_crypt(req, sg, result, 2);
-	
-if (crypto_ahash_digest(req))
-	fail();
-
-ahash_request_free(req);
-crypto_free_ahash(tfm);
