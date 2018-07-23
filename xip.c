500f9ff314cd6a3203bd0e2f81e06c2cd20f3e34
towander <137wander@gmail.com>
2018-07-23 21:10:58 +0800

    test

--- xip.c
+++ xip.c
@@ -440,8 +440,8 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		if(new_list->next!=&hash_map_addr_list && new_list->next!=NULL){
 			/* add physical address */
 			hash_map_addr_entry = list_entry(new_list->next, struct hash_map_addr, list);
-			while(false){
-				
+			while(hash_map_addr_entry->hashing_md5 != buf){
+				buf += bytes;
 			}
 			if(hash_map_addr_entry->hashing_md5 == buf){
 				// offset = (pos & (sb->s_blocksize - 1)); /* Within page */
