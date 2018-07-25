a9fa5867ebe3d30432815848fc41b2877c575e38
towander <137wander@gmail.com>
2018-07-25 17:34:37 +0800

    test

--- xip.c
+++ xip.c
@@ -913,7 +913,7 @@ static int __pmfs_xip_file_fault(struct vm_area_struct *vma,
 		err = pmfs_get_xip_mem(mapping, vmf->pgoff, 1, &xip_mem, &xip_pfn);
 	//end
 
-	pmfs_get_xip_mem(mapping, vmf->pgoff, 1, &xip_mem, &xip_pfn);
+	// pmfs_get_xip_mem(mapping, vmf->pgoff, 1, &xip_mem, &xip_pfn);
 
 	if (unlikely(err)) {
 		pmfs_dbg("[%s:%d] get_xip_mem failed(OOM). vm_start(0x%lx),"
