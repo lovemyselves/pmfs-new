5c0028cefac8c5d56fd7a9635d00d515f3c0d74f
towander <137wander@gmail.com>
2018-07-26 16:29:59 +0800

    test

--- xip.c
+++ xip.c
@@ -431,12 +431,20 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		if (bytes > count)
 			bytes = count;
 		
-		status = 0;
+		status = pmfs_get_xip_mem(mapping, index, 1, &xmem, &xpfn);
 
 		if (status)
 			break;
 
 		copied = bytes;
+
+		PMFS_START_TIMING(memcpy_w_t, memcpy_time);
+		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
+		copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
+		// copied = memcpy_to_nvmm((char *)xmem, offset, 
+		// hash_map_addr_entry->hashing_md5, hash_map_addr_entry->length);
+		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
+		PMFS_END_TIMING(memcpy_w_t, memcpy_time);
 		// printk("1 bytes:%lu", bytes);
 		// printk("1 copied:%lu", copied);
 		
@@ -459,18 +467,18 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 				// if (bytes > count)
 				// bytes = count;
 
-				status = pmfs_get_xip_mem(mapping, hash_map_addr_entry->pfn, 1, &xmem, &xpfn);
+				// status = pmfs_get_xip_mem(mapping, index, 1, &xmem, &xpfn);
 		
 				// if (status)
 				// 	break;
 	
-				PMFS_START_TIMING(memcpy_w_t, memcpy_time);
-				pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
-				// copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
-				copied = memcpy_to_nvmm((char *)xmem, offset, 
-				hash_map_addr_entry->hashing_md5, hash_map_addr_entry->length);
-				pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
-				PMFS_END_TIMING(memcpy_w_t, memcpy_time);
+				// PMFS_START_TIMING(memcpy_w_t, memcpy_time);
+				// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
+				// // copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
+				// copied = memcpy_to_nvmm((char *)xmem, offset, 
+				// hash_map_addr_entry->hashing_md5, hash_map_addr_entry->length);
+				// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
+				// PMFS_END_TIMING(memcpy_w_t, memcpy_time);
 				// printk("new_list hashing:%lu",hash_map_addr_entry->hashing);
 				if(hash_map_addr_entry->addr!=NULL)
 					kfree(hash_map_addr_entry->addr);
