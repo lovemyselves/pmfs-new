fbf519a03f721392b29bb47d4a1590b4713adcfe
towander <137wander@gmail.com>
2018-07-27 17:28:25 +0800

    test

--- xip.c
+++ xip.c
@@ -467,8 +467,6 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		    // // hash_map_addr_entry->hashing_md5, hash_map_addr_entry->length);
 		    // pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
 
-			xmem = kmalloc(hash_map_addr_entry->length, GFP_KERNEL);
-			copy_from_user(xmem, hash_map_addr_entry->hashing_md5, hash_map_addr_entry->length);
 			// copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
 				// if (status)
 				// 	break;
@@ -497,7 +495,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 				// printk("flush");
 				// printk("2 copied:%lu",copied);
 			}
-			goto dedup;
+			// goto dedup;
 		}
 		// PMFS_START_TIMING(memcpy_w_t, memcpy_time);
 		// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
