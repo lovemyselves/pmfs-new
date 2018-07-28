fdd6b1cdcc63aeb31aa2e4478fc4f86ab6f4bb6b
towander <137wander@gmail.com>
2018-07-28 16:39:52 +0800

    test

--- xip.c
+++ xip.c
@@ -482,6 +482,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 				// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
 				// PMFS_END_TIMING(memcpy_w_t, memcpy_time);
 				// printk("new_list hashing:%lu",hash_map_addr_entry->hashing);
+				
 				if(hash_map_addr_entry->addr!=NULL)
 					kfree(hash_map_addr_entry->addr);
 				hash_map_addr_entry->addr = (void*)xmem;
@@ -513,11 +514,11 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		// 	printk("No new data block");
 		// }
 
-		PMFS_START_TIMING(memcpy_w_t, memcpy_time);
-		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
-		copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
-		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
-		PMFS_END_TIMING(memcpy_w_t, memcpy_time);
+		// PMFS_START_TIMING(memcpy_w_t, memcpy_time);
+		// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
+		// copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
+		// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
+		// PMFS_END_TIMING(memcpy_w_t, memcpy_time);
 
 		/* if start or end dest address is not 8 byte aligned, 
 	 	 * __copy_from_user_inatomic_nocache uses cacheable instructions
