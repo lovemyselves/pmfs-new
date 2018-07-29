073a6b5c85bec6efbb1cf4db3ea446f803856d66
towander <137wander@gmail.com>
2018-07-29 13:18:22 +0800

    test

--- xip.c
+++ xip.c
@@ -445,9 +445,9 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		// printk("a __write call");
 
 		PMFS_START_TIMING(memcpy_w_t, memcpy_time);
-		// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
-		// copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
-		// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
+		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
+		copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
+		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
 		PMFS_END_TIMING(memcpy_w_t, memcpy_time);
 		printk("index:%lu",index);
 		// if(new_list->next!=&hash_map_addr_list && new_list->next!=NULL){
@@ -474,11 +474,11 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 			// if (status)
 			// 	break;
 			// printk("offset:%lu",(size_t)offset);
-			pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);	
-			copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
-		    // // copied = memcpy_to_nvmm((char *)xmem, offset, 
-		    // // hash_map_addr_entry->hashing_md5, hash_map_addr_entry->length);
-		    pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
+			// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);	
+			// copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
+		    // // // copied = memcpy_to_nvmm((char *)xmem, offset, 
+		    // // // hash_map_addr_entry->hashing_md5, hash_map_addr_entry->length);
+		    // pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
 
 			// copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
 				// if (status)
