5ce2462e06dca03ac1078a2e67d7668378487595
towander <137wander@gmail.com>
2018-07-29 13:16:29 +0800

    test

--- xip.c
+++ xip.c
@@ -434,11 +434,11 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		if (bytes > count)
 			bytes = count;
 		
-		// status = pmfs_get_xip_mem(mapping, index, 1, &xmem, &xpfn);
+		status = pmfs_get_xip_mem(mapping, index, 1, &xmem, &xpfn);
 		// status = 0;
 		
-		// if (status)
-		// 	break;
+		if (status)
+			break;
 
 		copied = bytes;
 
@@ -470,9 +470,9 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 				// if (bytes > count)
 				// bytes = count;
 
-			status = pmfs_get_xip_mem(mapping, index, 1, &xmem, &xpfn);
-			if (status)
-				break;
+			// status = pmfs_get_xip_mem(mapping, index, 1, &xmem, &xpfn);
+			// if (status)
+			// 	break;
 			// printk("offset:%lu",(size_t)offset);
 			pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);	
 			copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
