767971d747cab794d0c0cb67a11d5325322876f8
towander <137wander@gmail.com>
2018-07-30 16:25:08 +0800

    test

--- xip.c
+++ xip.c
@@ -898,7 +898,10 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	
 	/* don't zero-out the allocated blocks */
 	if(actual_num_blocks!=0){
-		printk("==========================j:%lu==========================",actual_num_blocks);
+		pritnk("\n");
+		printk("num_blocks:%lu",num_blocks);
+		printk("count:%lu",count);
+		printk("j:%lu==========================",actual_num_blocks);
 	// 	num_blocks = actual_num_blocks;
 	}
 
