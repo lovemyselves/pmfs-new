078eaebe6347e3c8fb26c589c11220eb39234f52
towander <137wander@gmail.com>
2018-07-30 14:46:11 +0800

    test

--- xip.c
+++ xip.c
@@ -446,7 +446,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 
 		PMFS_START_TIMING(memcpy_w_t, memcpy_time);
 		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 1);
-		copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
+		// copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
 		pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
 		PMFS_END_TIMING(memcpy_w_t, memcpy_time);
 		printk("index:%lu",index);
@@ -480,7 +480,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		    // // // hash_map_addr_entry->hashing_md5, hash_map_addr_entry->length);
 		    // pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
 
-			// copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
+			copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
 				// if (status)
 				// 	break;
 	
