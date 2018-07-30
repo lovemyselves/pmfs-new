9312e7a0c44f84a2574c8a2342d1bf489852ba68
towander <137wander@gmail.com>
2018-07-30 14:50:08 +0800

    test

--- xip.c
+++ xip.c
@@ -480,7 +480,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 		    // // // hash_map_addr_entry->hashing_md5, hash_map_addr_entry->length);
 		    // pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
 
-			copied = memcpy_to_nvmm((char *)xmem, offset+64, buf, bytes);
+			copied = memcpy_to_nvmm((char *)xmem, offset, buf, bytes);
 				// if (status)
 				// 	break;
 	
@@ -495,7 +495,7 @@ __pmfs_xip_file_write(struct address_space *mapping, const char __user *buf,
 				// pmfs_xip_mem_protect(sb, xmem + offset, bytes, 0);
 				// PMFS_END_TIMING(memcpy_w_t, memcpy_time);
 				// printk("new_list hashing:%lu",hash_map_addr_entry->hashing);
-				copy_from_user(xmem+offset,buf+128,bytes);
+				// copy_from_user(xmem+offset,buf+128,bytes);
 				if(hash_map_addr_entry->addr!=NULL)
 					kfree(hash_map_addr_entry->addr);
 				hash_map_addr_entry->addr = (void*)xmem;
