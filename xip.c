31d4a1b293f4b318be3aa7efaf0b7a7103d9fa99
towander <137wander@gmail.com>
2018-08-09 22:16:10 +0800

    test

--- xip.c
+++ xip.c
@@ -57,8 +57,8 @@ char *do_digest(char* code, size_t len){
 	struct shash_request *req;
 	
 	tfm = crypto_alloc_shash("md5", 0, CRYPTO_ALG_ASYNC);
-	if (IS_ERR(tfm))
-		fail();
+	// if (IS_ERR(tfm))
+	// 	fail();
 
 	// ahash_request_set_callback(req, 0, NULL, NULL);
 	// ahash_request_set_crypt(req, sg, result, 2);
@@ -83,17 +83,17 @@ plaintext_to_sha1(unsigned char *hash, const char *plaintext, unsigned int len)
 //             "bytes.\n", len, PAGE_SIZE);
 //     return -EINVAL;
 //   }
-  tfm = crypto_alloc_shash("sha1", CRYPTO_TFM_REQ_MAY_SLEEP);
+  tfm = crypto_alloc_tfm("sha1", 0, CRYPTO_TFM_REQ_MAY_SLEEP);
 //   if (tfm == NULL) {
 //     seclvl_printk(0, KERN_ERR,
 //             "Failed to load transform for SHA1\n");
 //     return -EINVAL;
 //   }
   sg_init_one(&sg, (u8 *)plaintext, len);
-  crypto_digest_init(tfm);
-  crypto_digest_update(tfm, &sg, 1);
-  crypto_digest_final(tfm, hash);
-  crypto_free_shash(tfm);
+//   crypto_digest_init(tfm);
+//   crypto_digest_update(tfm, &sg, 1);
+//   crypto_digest_final(tfm, hash);
+//   crypto_free_shash(tfm);
   return 0;
 }
 
