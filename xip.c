88f2222bfed63cd8692cc94f39dcc0979037baab
towander <137wander@gmail.com>
2018-08-30 20:24:58 +0800

    test

--- xip.c
+++ xip.c
@@ -64,13 +64,15 @@ void new_unused_dedupnode(struct super_block *sb){
 	unsigned long blocknr;
 	struct dedupnode *dnode;
 	unsigned offset = 0;
+	void *xmem;
 	struct dedup_index *dindex = pmfs_get_block(sb, DEDUP_HEAD<<PAGE_SHIFT);
 	
 	pmfs_new_block(sb, &blocknr, PMFS_BLOCK_TYPE_4K, 1);
+	xmem = pmfs_get_block(sb, blocknr<<PAGE_SHIFT);
 	
 	while(offset + DEDUPNODE_SIZE <4096)
 	{	
-		dnode = xmem + offset;
+		dnode =  + offset;
 		// INIT_LIST_HEAD(&hash_map_addr_temp->list);
 		// list_add_tail(&hash_map_addr_temp->list, &hash_map_addr_list);
 		INIT_LIST_HEAD(&dnode->list);
