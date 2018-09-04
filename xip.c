739232410e6932100341d05253d3b2febcfd908a
towander <137wander@gmail.com>
2018-09-04 16:12:33 +0800

    test

--- xip.c
+++ xip.c
@@ -762,17 +762,17 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 		printk("pmfs write 0");
 		rnode_insert_ret = refnode_insert(sb, rnode);
 		printk("pmfs write 0.1");
-		if(rnode_insert_ret){
-			rnode = rnode_insert_ret;
-			if(rnode->dnode->count>1)
-				//update COW
-				overwrite_flag = 1;
-			else{
-				overwrite_flag = 2;
-				rnode->dnode->count = 1;
-				//update in-place		
-			}
-		}
+		// if(rnode_insert_ret){
+		// 	rnode = rnode_insert_ret;
+		// 	if(rnode->dnode->count>1)
+		// 		//update COW
+		// 		overwrite_flag = 1;
+		// 	else{
+		// 		overwrite_flag = 2;
+		// 		rnode->dnode->count = 1;
+		// 		//update in-place		
+		// 	}
+		// }
 		printk("pmfs write 1");
 
 		//alloc and init dnode
