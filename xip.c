02b627a44dcb94ae896ae261ad88415407495cd4
towander <137wander@gmail.com>
2018-09-06 11:11:21 +0800

    test

--- xip.c
+++ xip.c
@@ -1128,7 +1128,7 @@ static int __pmfs_xip_file_fault(struct vm_area_struct *vma,
 	// 	}
 	
 	if(vmf->pgoff!=0 && rnode_hit==true){
-		rnode = list_entry(last_rnode_list->next, struct refnode, list);
+		rnode = list_entry(last_rnode_list->prev, struct refnode, list);
 		printk("xip file fault function try hit");
 		printk("inode->ino:%u",inode->i_ino);
 		printk("rnode->ino:%u",rnode->ino);
