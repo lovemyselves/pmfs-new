1fc7f79757821bb3d73f9710bc244187559101ce
towander <137wander@gmail.com>
2018-09-21 22:24:43 +0800

    test

--- inode.c
+++ inode.c
@@ -993,7 +993,7 @@ out:
 		printk("count:%lu", rnode->dnode->count);
 		if(--(rnode->dnode->count) == 0){
 			printk("free block");
-			pmfs_free_block(sb, dnode->blocknr, PMFS_BLOCK_TYPE_4K);
+			// pmfs_free_block(sb, dnode->blocknr, PMFS_BLOCK_TYPE_4K);
 			printk("free dnode");
 		}
 		printk("free rnode");
