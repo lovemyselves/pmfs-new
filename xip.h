fc0f61e2483596fe3c0967505d1f14648e7775ad
towander <137wander@gmail.com>
2018-10-01 15:30:22 +0800

    test

--- xip.h
+++ xip.h
@@ -20,7 +20,7 @@ int pmfs_xip_file_mmap(struct file *file, struct vm_area_struct *vma);
 //dedup part start
 struct refnode *refnode_search(struct super_block *sb
 ,unsigned ino, unsigned long index);
-// bool free_dedupnode(struct super_block *sb, struct dedupnode *dnode);
+bool free_dedupnode(struct super_block *sb, struct dedupnode *dnode);
 
 bool free_refnode(struct super_block *sb, struct refnode *rnode);
 //dedup part end
