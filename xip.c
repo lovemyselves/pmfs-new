61b100c105481b57b1c7a646aefbee60408ce3f2
towander <137wander@gmail.com>
2018-10-23 21:56:52 +0800

    test

--- xip.c
+++ xip.c
@@ -910,7 +910,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 		dnode->hashval = hashing;
 		dnode->count = 1;
 		dnode->strength_hash_status = 0;
-		// strength_hash(dnode->strength_hashval, xmem, block_len);
+		strength_hash(dnode->strength_hashval, xmem, block_len);
 		// dnode->strength_hash_status = 1;
 		// memset(dnode->strength_hashval, 0, sizeof(char)<<16); 
 
