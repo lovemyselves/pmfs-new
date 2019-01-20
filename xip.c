c6637b3162d5cce417d587a8bdb50f6724bd16e1
towander <137wander@gmail.com>
2019-01-20 22:06:27 +0800

    Update xip.c

--- xip.c
+++ xip.c
@@ -405,19 +405,19 @@ void short_userspace_hash(size_t __user *hashing, char __user *buf, size_t len)
 	if(*hashing!=0)
 		return false;
 				 
-	// if(data_remainder!=0)
-	// 	memcpy(hashing, buf+len-data_remainder, data_remainder);
+	if(data_remainder!=0)
+		memcpy(hashing, buf+len-data_remainder, data_remainder);
 
-	// for(k=0;(k+sizeof(size_t))<len;){
-	// 	*hashing += *(size_t*)(buf + k);
-	// 	*hashing += (*hashing << 3);
-	// 	*hashing ^= (*hashing >> 2);
-	// 	if(thick_internal_count>0){
-	// 		k += sizeof(size_t);	
-	// 		thick_internal_count--;
-	// 	}
-	// 	k += (thin_internal<<3);
-	// }
+	for(k=0;(k+sizeof(size_t))<len;){
+		*hashing += *(size_t*)(buf + k);
+		*hashing += (*hashing << 3);
+		*hashing ^= (*hashing >> 2);
+		if(thick_internal_count>0){
+			k += sizeof(size_t);	
+			thick_internal_count--;
+		}
+		k += (thin_internal<<3);
+	}
 }
 
 bool strength_hash(char *result, char* data, size_t len){
