be1ac7bdbc167795f6f6c05d00ce322b26e47b9d
towander <137wander@gmail.com>
2019-01-21 22:22:33 +0800

    Update xip.c

--- xip.c
+++ xip.c
@@ -207,7 +207,7 @@ struct dedupnode *dedupnode_tree_update(struct super_block *sb
 	if(flag){
 		dnode_entry = alloc_dedupnode(sb);
 
-		printk("onlypoint:%d",sizeof(struct dedupnode_onlypoint));
+		printk("onlypoint:%d",(int)sizeof(struct deudpnode_onlypoint));
 
 		memcpy(dnode_entry, dnode_new, 56);//sizeof(struct dedupnode)-sizeof(struct list_head)-sizeof(struct rb_node) equal to 56
 		// kfree(dnode_new);
