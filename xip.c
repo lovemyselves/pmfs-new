47e49e61ea5e6c1338fcf2496c34e23c8b12bdb7
towander <137wander@gmail.com>
2019-01-23 21:36:34 +0800

    Update xip.c

--- xip.c
+++ xip.c
@@ -330,12 +330,12 @@ bool strength_hash(char *result, char* data, size_t len){
 	// crypto_shash_final(desc, result);
 	// crypto_free_shash(desc->tfm);
 	int i,j,cycles;
-	memset(resuslt, 0, 16);
+	memset(result, 0, 16);
 	memcpy(result, data, len-(len&15)); //remainder divided by 16
 	cycles = len>>4;
 	for(i=0;i<cycles;i++){
-		*(long long)result += *(long long)(data + i<<4);
-		*(long long)result ^= *(long long)result >> 1;
+		*(long long*)result += *(long long*)(data + i<<4);
+		*(long long*)result ^= *(long long*)result >> 1;
 	}
 
 
