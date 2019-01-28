11f233d326cd6a6a875df0915f0a08389c06bf54
towander <137wander@gmail.com>
2019-01-28 20:58:08 +0800

    Update xip.c

--- xip.c
+++ xip.c
@@ -132,8 +132,8 @@ bool free_dedupnode(struct super_block *sb, void *dedupnode){
 		return false;
 		
 	dnode->flag = 0;
-	pmfs_free_block(sb, dnode->blocknr, PMFS_BLOCK_TYPE_4K);
-	rb_erase(&dnode->node, droot);
+	// pmfs_free_block(sb, dnode->blocknr, PMFS_BLOCK_TYPE_4K);
+	// rb_erase(&dnode->node, droot);
 	list_move_tail(&dnode->list, &dindex->hma_unused);
 	return true;
 }
