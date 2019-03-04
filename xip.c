39b342c19581d452ac3139d70bca98a072e9e58b
towander <137wander@gmail.com>
2019-03-04 18:39:47 +0800

    Update xip.c

--- xip.c
+++ xip.c
@@ -813,7 +813,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 	i = count;
 	dedup_offset = offset;
 
-	if((start_blk&63) && dnode_hit<=-32){
+	if((start_blk&32) && dnode_hit<=-32){
 			goto sequential_nondup;
 	}
 	for(j = 0; j < num_blocks; j++ ){
