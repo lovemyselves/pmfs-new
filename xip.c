0b0c54ce05049f53a72c1a8cde34807166597c8c
towander <137wander@gmail.com>
2019-03-07 22:57:14 +0800

    Update xip.c

--- xip.c
+++ xip.c
@@ -904,8 +904,7 @@ ssize_t pmfs_xip_file_write(struct file *filp, const char __user *buf,
 		xmem = kmalloc(pmfs_inode_blk_size(pi), GFP_KERNEL);
 		copy_from_user(xmem + dedup_offset, buf+count-i, block_len);
 		dedup_offset = 0;
-		// if( (start_blk&3 || j&31) && dnode_hit<=-32){
-			if(false){
+		if( (start_blk&3 || j&31) && dnode_hit<=-32){
 			// printk("skip:%d", dnode_hit);
 			dnode->strength_hash_status = 2;
 			goto strength_hashing_hit;
